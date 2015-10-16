97598a406b34e5c8058087ab4e6d4c1e1de3790a
sigveah <sigveah@gmail.com>
2015-10-16 13:46:03 +0200

    Update Oppgave6.java

--- Oppgave6.java
+++ Oppgave6.java
@@ -4,7 +4,12 @@ public class Oppgave6
   public static void main(String[]args)throws Exception
   {
     Ordliste liste = new Ordliste();
-    liste.lesBok("scarlet.txt");
+    liste.lesBok("scarlet.text");
+    System.out.println(liste.antallOrd());
+
+
+
+
 
 
   }
