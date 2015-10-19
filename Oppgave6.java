9ad95bc2b674ff6ed9f879a9b42f7abb34763067
sigveah <sigveah@gmail.com>
2015-10-19 23:45:59 +0200

    Update Oppgave6.java

--- Oppgave6.java
+++ Oppgave6.java
@@ -5,13 +5,9 @@ public class Oppgave6
   {
     Ordliste liste = new Ordliste();
     liste.lesBok("scarlet.text");
-    System.out.println(liste.antallOrd());
-
-
-
-
-
-
+    System.out.println("Det er totalt " + liste.antallOrd() + " ulike ord i teksten.");
+    System.out.println("Holmes er skrevet " + liste.antallForekomster("holmes") + " ganger i teksten.");
+    System.out.println("Elementary er skrevet " + liste.antallForekomster("elementary") + " ganger i teksten.");
+    System.out.println("Vanligste ord er " + liste.vanligste().toString());
   }
-
 }
